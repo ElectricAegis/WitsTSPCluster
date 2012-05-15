2068b3e865ab886ed5df8643b3336633b97b0fce
Kailesh Ramjee <kaileshramjee@gmail.com>
2012-05-15 14:48:31 +0200

    Updated licensing information for the TSP source files

--- src/list.cpp
+++ src/list.cpp
@@ -1,9 +1,20 @@
-/* list.C 
-
-      Routines to manage a singly-linked list of "things".
-      It's a priority queue, but it could be more efficient 
-      implemented  as a heap.
-*/
+/** list.C
+ *
+ *      Routines to manage a singly-linked list of "things".
+ *      It's a priority queue, but it could be more efficient
+ *      implemented  as a heap.
+ *
+ *      Author: Hermanus L. Brummer
+ *
+ *      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *      OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ *      NON-INFRINGEMENT. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ *      ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE FOR ANY DAMAGES OR
+ *      OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, ARISING
+ *      FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *      OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include <stdlib.h>
 #include "list.h"
